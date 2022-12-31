c36c98e580bbdcc2bb109598186aa8a3270cf37e
jobin <jobinnelson369@gmail.com>
2022-12-31 16:43:57 +0530

    reformatted the code

---
+++ test_updater.py
@@ -0,0 +1,7 @@
+'''This module tests the updater'''
+from updater import is_env_set
+
+def test_env_is_set():
+    assert is_env_set()
+
+
--- updater.py
+++ updater.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''This script sends email updates''' 
+from __future__ import annotations
 from dotenv import load_dotenv
 from datetime import datetime
 from pathlib import Path
 from email.message import EmailMessage
-import smtplib, ssl, os
+import smtplib, ssl, os, base64
 
 load_dotenv()
 
@@ -17,34 +18,38 @@ today = datetime.today()
 daily_update_path = Path.home() / 'playground' / 'dev' / 'illumina' / 'daily_updates' / f'{today:%Y-%m-%d}.txt'
 
 def main() -> int:
+    if not is_env_set():
+        print('Environment variables are not set properly')
+        return 1
+
+    email_to_sent = gen_email()
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, base64.b64decode(email_password).decode().strip())
+        smtp.sendmail(email_sender, email_receiver, email_to_sent.as_string())
+        print(f'Daily email update send for {today:%Y-%m-%d}')
+
+    return 0
+
+def is_env_set() -> bool:
+    return all(e for e in [email_sender, email_password, email_receiver])
+
+def gen_email() -> EmailMessage:
     subject = 'Daily update'
+    body = ''
     if daily_update_path.exists():
         body = daily_update_path.read_text()
-    else:
-        body = 'No updates for today'
 
-    if not email_password:
-        print('No email password is found')
-        return 1
-
-    if not email_receiver:
-        print('No email receiver is found')
-        return 1
+    body = body or 'No updates for today'
 
     em = EmailMessage()
     em['From'] = email_sender
     em['To'] = email_receiver
     em['Subject'] = subject
     em.set_content(body)
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
-
-    print(f'Daily email update send for {today:%Y-%m-%d}')
-    return 0
+    return em
 
 if __name__ == '__main__':
     raise SystemExit(main())
