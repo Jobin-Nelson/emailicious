f2ba94311be1bd4af85ad1a7f72b92c05e5d8926
jobin <jobinnelson369@gmail.com>
2023-01-07 22:35:51 +0530

    added tests for all functions

--- test_updater.py
+++ test_updater.py
@@ -1,7 +1,86 @@
 '''This module tests the updater'''
-from updater import is_env_set
+from email.message import EmailMessage
+import urllib.request, os
+from dotenv import load_dotenv
+import updater
+import pytest
 
-def test_env_is_set():
-    assert is_env_set()
+# @pytest.fixture(autouse=True)
+# def new_updater():
+#     load_dotenv()
+#
+#     updater.EMAIL_SENDER = os.getenv('EMAIL_SENDER')
+#     updater.EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+#     updater.EMAIL_RECEIVER = os.getenv('EMAIL_RECEIVER')
+#     return updater
+
+def has_connection() -> bool:
+    url = 'http://google.com'
+    try:
+        urllib.request.urlopen(url, timeout=3)
+    except Exception:
+        return False
+    return True
+
+def test_env_is_empty():
+    updater.EMAIL_SENDER = ''
+    assert updater.is_env_set() == False
+
+def test_env_not_found():
+    updater.EMAIL_SENDER = None
+    assert updater.is_env_set() == False
+
+def test_env_all_set():
+    updater.EMAIL_SENDER = 'sender@gmail.com'
+    updater.EMAIL_PASSWORD = 'senderpass$'
+    updater.EMAIL_RECEIVER = 'receiver@gmail.com'
+    assert updater.is_env_set()
+
+def test_email_body_if_path_not_exist(tmp_path):
+    updater.DAILY_UPDATE_PATH = tmp_path / 'non-existing-dir' / 'non-existing-file.txt'
+    assert updater.get_body() is None
+
+def test_email_body_if_path_exist(tmp_path):
+    tmp_file = tmp_path / 'test.txt'
+    body = 'Testing get_body() function\n'
+    tmp_file.write_text(body)
+    updater.DAILY_UPDATE_PATH = tmp_file
+    assert updater.get_body() == body
+
+def test_gen_email():
+    assert isinstance(updater.gen_email(), EmailMessage)
+
+def test_gen_email_body_if_content_exist(tmp_path):
+    tmp_file = tmp_path / 'test.txt'
+    body = 'Testing get_body() function\n'
+    tmp_file.write_text(body)
+    updater.DAILY_UPDATE_PATH = tmp_file
+    em = updater.gen_email()
+    assert em.get_content() == body
 
 
+def test_gen_email_body_if_content_empty(tmp_path):
+    tmp_file = tmp_path / 'test.txt'
+    body = ''
+    tmp_file.write_text(body)
+    updater.DAILY_UPDATE_PATH = tmp_file
+    em = updater.gen_email()
+    assert em.get_content() == updater.PLACEHOLDER_BODY
+
+@pytest.mark.skipif(not has_connection(), reason='No internet connection')
+def test_send_email(tmp_path, capsys):
+    tmp_file = tmp_path / 'test.txt'
+    body = 'Testing send_email() function\n'
+    tmp_file.write_text(body)
+
+    load_dotenv()
+
+    updater.EMAIL_SENDER = os.getenv('EMAIL_SENDER')
+    updater.EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+    updater.EMAIL_RECEIVER = updater.EMAIL_SENDER
+
+    updater.DAILY_UPDATE_PATH = tmp_file
+
+    updater.send_email(updater.gen_email())
+    stdout, _ = capsys.readouterr()
+    assert stdout == f'Daily email update send for {updater.TODAY:%Y-%m-%d}\n'
--- updater.py
+++ updater.py
@@ -5,51 +5,67 @@ from dotenv import load_dotenv
 from datetime import datetime
 from pathlib import Path
 from email.message import EmailMessage
-import smtplib, ssl, os, base64
+import smtplib, ssl, os, base64, sys
 
 load_dotenv()
 
-email_sender = os.getenv('EMAIL_SENDER')
-email_password = os.getenv('EMAIL_PASSWORD')
-email_receiver = os.getenv('EMAIL_RECEIVER')
+EMAIL_SENDER = os.getenv('EMAIL_SENDER')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_RECEIVER = os.getenv('EMAIL_RECEIVER')
 
-today = datetime.today()
+TODAY = datetime.today()
 
-daily_update_path = Path.home() / 'playground' / 'dev' / 'illumina' / 'daily_updates' / f'{today:%Y-%m-%d}.txt'
+DAILY_UPDATE_PATH = Path.home() / 'playground' / 'dev' / 'illumina' / 'daily_updates' / f'{TODAY:%Y-%m-%d}.txt'
+
+PLACEHOLDER_BODY = 'No updates for today\n'
 
 def main() -> int:
+    '''Sends an email update for today'''
     if not is_env_set():
-        print('Environment variables are not set properly')
+        print('Environment variables are not set properly', file=sys.stderr)
         return 1
 
     email_to_sent = gen_email()
 
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, base64.b64decode(email_password).decode().strip())
-        smtp.sendmail(email_sender, email_receiver, email_to_sent.as_string())
-        print(f'Daily email update send for {today:%Y-%m-%d}')
+    send_email(email_to_sent)
 
     return 0
 
 def is_env_set() -> bool:
-    return all(e for e in [email_sender, email_password, email_receiver])
+    '''Checks if required environment variables are set'''
+    return all(e for e in [EMAIL_SENDER, EMAIL_PASSWORD, EMAIL_RECEIVER])
 
 def gen_email() -> EmailMessage:
+    '''Generates today's email'''
     subject = 'Daily update'
-    body = ''
-    if daily_update_path.exists():
-        body = daily_update_path.read_text()
 
-    body = body or 'No updates for today'
+    body = get_body() or PLACEHOLDER_BODY
 
     em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_receiver
+    em['From'] = EMAIL_SENDER
+    em['To'] = EMAIL_RECEIVER
     em['Subject'] = subject
     em.set_content(body)
     return em
+
+def get_body() -> str | None:
+    '''Returns the file contents for today'''
+    if DAILY_UPDATE_PATH.exists():
+        return DAILY_UPDATE_PATH.read_text()
+
+def send_email(email_to_sent: EmailMessage) -> None:
+    '''Sends the email'''
+    context = ssl.create_default_context()
+
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(EMAIL_SENDER, base64.b64decode(EMAIL_PASSWORD).decode().strip())
+            smtp.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, email_to_sent.as_string())
+    except smtplib.SMTPException as e:
+        print(f'Could not send email, encountered an exception {e}', file=sys.stderr)
+        sys.exit(1)
+    else:
+        print(f'Daily email update send for {TODAY:%Y-%m-%d}')
 
 if __name__ == '__main__':
     raise SystemExit(main())
